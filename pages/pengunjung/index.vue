9b1b63a9e663338be1f4c2f93a2f0d6374e470f1
alficlanjr <alfisabina@gmail.com>
2024-05-13 11:37:17 +0700

    update

--- pages/pengunjung/index.vue
+++ pages/pengunjung/index.vue
@@ -24,7 +24,7 @@
                     <tr v-for="(visitor,i) in visitors" :key="i">
                         <td>{{ i+1 }}.</td>
                         <td>{{ visitor.nama }}</td>
-                        <td>{{ visitor.nama.keanggotaan }}</td>
+                        <td>{{ visitor.keanggotaan.nama }}</td>
                         <td>{{ visitor.tgl }}, {{ visitor.waktu }}</td>
                         <td>{{ visitor.keperluan.nama }}</td>
                     </tr>
